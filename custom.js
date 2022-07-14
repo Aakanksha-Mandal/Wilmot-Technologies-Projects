8288fdd82e4f6d525b574fb177f4711586e89ce3
Gen X Coding <81123859+GenXCoding@users.noreply.github.com>
2022-07-14 15:53:49 -0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,3 @@
-
 async function getUsers(page_no, per_page){
 	let response = await axios('https://reqres.in/api/users?page='+page_no+'&per_page='+per_page)
 
@@ -14,4 +13,6 @@ https://w3collective.com/fetch-display-api-data-javascript/
 
 API Link
 https://reqres.in/api/users?page=1&per_page=10
+
+https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/#:~:text=Approach%3A%20First%20make%20the%20necessary,by%20await%20fetch()%20method.
 */
