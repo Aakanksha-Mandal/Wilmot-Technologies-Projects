8c047c43bb0aa8963c33368520bc2b09162ac011
Gen X Coding <81123859+GenXCoding@users.noreply.github.com>
2022-07-27 15:51:28 -0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -63,7 +63,7 @@ const addToDo = () => {
 }
 
 function save(todolist = null) {
-    const list = todolist??todoList;
+    const list = todolist??todoList
     let stringified = JSON.stringify(list)
     localStorage.setItem("todoList", stringified)
 }
@@ -79,16 +79,13 @@ function load() {
 }
 
 function remove(ele) {
-    const remove_id = ele.getAttribute('data-id');
-    console.log(remove_id)
+    const remove_id = ele.getAttribute('data-id')
     let retrieved = localStorage.getItem("todoList")
     todoList = JSON.parse(retrieved)
     if(todoList?.length > 0) {
         const todolist = todoList.filter((todoItem)=>{
             return todoItem.id != remove_id
         })
-        
-        console.log(todolist)
         save(todolist)
     }
     return false
@@ -97,11 +94,13 @@ function remove(ele) {
 const generateTodoItem = (tableBody, todoItem) => {
     tableBody.innerHTML += `
             <tr data-id="${todoItem.id}">
-                <td onClick="doneToDoItem(this)">${todoItem.task}</td>
-                <td onClick="doneToDoItem(this)">${getFormatedDate(todoItem.date)}</td> 
-                <td onClick="doneToDoItem(this)">${todoItem.time}</td>
-                <td onClick="doneToDoItem(this)">${todoItem.priority}</td>
-                <td onClick="doneToDoItem(this)">${todoItem.details}</td>
+                <td class="edit_btn"><i class="fas fa-edit" onclick="editTodoItem(this)"></i></td>
+                <td onClick="strikeToDoItem(this)">${todoItem.task}</td>
+                <td onClick="strikeToDoItem(this)">${getFormatedDate(todoItem.date)}</td> 
+                <td onClick="strikeToDoItem(this)">${todoItem.time}</td>
+                <td onClick="strikeToDoItem(this)">${todoItem.priority}</td>
+                <td onClick="strikeToDoItem(this)">${todoItem.details}</td>
+                <td class="save_btn"><div class="save_btn_wrap hide"><i class="fas fa-save" onclick="saveTodoItem(this)"></i></div></td>
                 <td class="remove_btn"><i class="fas fa-times" onclick="removeTodoItem(this.parentNode.parentNode)"></i></td>
             </tr>`
 
@@ -134,6 +133,10 @@ function filterResults() {
         timeTo: timeToFilter.value,
         priority: priorityFilter.value,
     }
+    if (filters.dataFrom == undefined && filters.dataTo == undefined && filters.timeFrom == "" && filters.timeTo == "" && filters.priority == "") {
+        alert("Filter Options are all blank.")
+        return
+    }
 
     let filteredResults = todoList.filter((item, index) => {
         if (item.date == filters.dateFrom) {
@@ -144,21 +147,21 @@ function filterResults() {
             return true
         }
         if (item.time == filters.timeFrom) {
-            return item.time == filters.timeFrom
+            return true
         } else if (item.time >= filters.timeFrom && item.time <= filters.timeTo) {
-            return item.time >= filters.timeFrom && item.time <= filters.timeTo
+            return true
         } else if (item.time == filters.timeTo) {
-            return item.time == filters.timeFrom
+            return true
         }
         if (item.priority == filters.priority) {
-            return item.priority == filters.priority
-        }
-        if (filters.dateFrom != "" || filters.dateTo != "" || filters.timeFrom != "" || filters.timeTo != "" || filters.priority != "") {
-            const tableBody = table.tBodies[0]
-            tableBody.innerHTML = ''
-            generateTodoList(filteredResults)
+            return true
         }
     })
+
+    const tableBody = table.tBodies[0]
+    tableBody.innerHTML = ''
+    generateTodoList(filteredResults)
+       
 }
 
 function getFormatedDate(date){
@@ -172,8 +175,13 @@ function getFormatedDate(date){
 }
 
 
-function doneToDoItem(ele) {
-    ele.parentNode.classList.toggle("done")
+function strikeToDoItem(ele) {
+    console.log(ele)
+    const isEditable = ele.getAttribute('contenteditable');
+    console.log(isEditable)
+    if(!isEditable){
+        ele.parentNode.classList.toggle("strike")
+    }
 }
 
 function removeTodoItem(ele) {
@@ -182,196 +190,38 @@ function removeTodoItem(ele) {
     remove(ele)
 }
 
-function clear() {
-    document.getElementById("task").value = ""
-    document.getElementById("date").value = ""
-    document.getElementById("time").value = ""
-    document.getElementById("priority").value = ""
-    document.getElementById("details").value = ""
-}
-
-load()let counter = 0
-let todoList = []
-const task = document.getElementById("task")
-const toDoBox = document.getElementById("to-do-box")
-const date = document.getElementById("date")
-const dataFilter = document.getElementById("dateFilter")
-const time = document.getElementById("time")
-const priority = document.getElementById("priority")
-const details = document.getElementById("details")
-const addButton = document.getElementById("add_btn")
-const table = document.getElementById("table")
-const dateFromFilter = document.getElementById("dateFromFilter")
-const dateToFilter = document.getElementById("dateToFilter")
-const timeFromFilter = document.getElementById("timeFromFilter")
-const timeToFilter = document.getElementById("timeToFilter")
-const priorityFilter = document.getElementById("priorityFilter")
-let priorityRegex = /high|medium|low/i
-
-addButton.addEventListener(
-    "click",
-    function() {
-        validate()
-    }
-) 
-
-function validate() {
-    if (task.value == "") {
-        alert("Task cannot be blank")
-        return false
-    }
-    if (date.value == "") {
-        alert("Date cannot be blank.")
-        return false
-    }
-    if (time.value == "") {
-        alert("Time cannot be blank.")
-        return false
-    }
-    if (priority.value == "" || (priorityRegex.test(priority.value)) == false) {
-        alert("Priority has to be high, medium, or low.")
-        return false
-    }
-    addToDo()
-    return true
-}
-
-
-const addToDo = () => { 
-    const todoItem = {
-        id: counter,
-        task: task.value,
-        date: date.value,
-        time: time.value,
-        priority: priority.value,
-        details: details.value
-    }
-
-    todoList.push(todoItem)
-    counter++
-    const tableBody = table.tBodies[0]
-    save()
-    generateTodoItem(tableBody, todoItem)
-}
-
-function save(todolist = null) {
-    const list = todolist??todoList;
-    let stringified = JSON.stringify(list)
-    localStorage.setItem("todoList", stringified)
-}
-
-function load() {
-    let retrieved = localStorage.getItem("todoList")
-    todoList = JSON.parse(retrieved)
-    if (todoList?.length > 0) {
-        generateTodoList(todoList)
-    }else{
-        todoList= []
-    }
-}
-
-function remove(ele) {
-    const remove_id = ele.getAttribute('data-id');
-    console.log(remove_id)
-    let retrieved = localStorage.getItem("todoList")
-    todoList = JSON.parse(retrieved)
-    if(todoList?.length > 0) {
-        const todolist = todoList.filter((todoItem)=>{
-            return todoItem.id != remove_id
-        })
-        
-        console.log(todolist)
-        save(todolist)
-    }
-    return false
-}
-
-const generateTodoItem = (tableBody, todoItem) => {
-    tableBody.innerHTML += `
-            <tr data-id="${todoItem.id}">
-                <td onClick="doneToDoItem(this)">${todoItem.task}</td>
-                <td onClick="doneToDoItem(this)">${getFormatedDate(todoItem.date)}</td> 
-                <td onClick="doneToDoItem(this)">${todoItem.time}</td>
-                <td onClick="doneToDoItem(this)">${todoItem.priority}</td>
-                <td onClick="doneToDoItem(this)">${todoItem.details}</td>
-                <td class="remove_btn"><i class="fas fa-times" onclick="removeTodoItem(this.parentNode.parentNode)"></i></td>
-            </tr>`
-
-     /* WAY 2 TO DELETE LIST ITEM
-    const closeBtn = tableBody.querySelector("tr").querySelector("i")
+function editTodoItem(ele) {
+    const parent_eles = ele.parentNode.parentNode.parentNode.childNodes
     
-    closeBtn.addEventListener(
-        "click",
-        function(e) {
-            e.target.parentNode.parentNode.remove()
+    const saveBtn = ele.parentNode.parentNode.getElementsByClassName('save_btn_wrap')[0]
+    saveBtn.classList.remove('hide')
+    saveBtn.classList.add('show')
+    
+    const child_eles = ele.parentNode.parentNode.childNodes
+    for(let i = 0; i <= child_eles.length; i++) {
+        const ele = child_eles[i]
+        if(ele?.nodeName  == 'TD' && ele?.className != "edit_btn" && ele?.className != "remove_btn" && ele?.className != "save_btn"){
+            ele.setAttribute('contenteditable', true)
         }
-    )*/
-
-    clear()
-}
-
-function generateTodoList(list){
-    const tableBody = table.tBodies[0]
-    list?.map((todoItem, index)=>{
-        generateTodoItem(tableBody, todoItem)
-    })
-
-}
-
-function filterResults() {
-    const filters = {
-        dateFrom: dateFromFilter.value,
-        dateTo: dateToFilter.value,
-        timeFrom: timeFromFilter.value,
-        timeTo: timeToFilter.value,
-        priority: priorityFilter.value,
     }
-
-    let filteredResults = todoList.filter((item, index) => {
-        if (item.date == filters.dateFrom) {
-            return true
-        } else if (item.date >= filters.dateFrom && item.date <= filters.dateTo) {
-            return true
-        } else if (item.date == filters.dateTo) {
-            return true
-        }
-        if (item.time == filters.timeFrom) {
-            return item.time == filters.timeFrom
-        } else if (item.time >= filters.timeFrom && item.time <= filters.timeTo) {
-            return item.time >= filters.timeFrom && item.time <= filters.timeTo
-        } else if (item.time == filters.timeTo) {
-            return item.time == filters.timeFrom
-        }
-        if (item.priority == filters.priority) {
-            return item.priority == filters.priority
-        }
-        if (filters.dateFrom != "" || filters.dateTo != "" || filters.timeFrom != "" || filters.timeTo != "" || filters.priority != "") {
-            const tableBody = table.tBodies[0]
-            tableBody.innerHTML = ''
-            generateTodoList(filteredResults)
-        }
-    })
 }
 
-function getFormatedDate(date){
-    let dateObj = new Date(date + "EST")
-    let formattedDate = dateObj.toLocaleString("en-GB", {
-        month: "long",
-        day: "numeric",
-        year: "numeric"
-    })
-    return formattedDate
-}
+function saveTodoItem(ele) {
+    console.log(ele.parentNode.parentNode.parentNode.childNodes)
+    const child_eles = ele.parentNode.parentNode.parentNode.childNodes
+
+    const saveBtn = ele.parentNode.parentNode.getElementsByClassName('save_btn_wrap')[0]
+    saveBtn.classList.remove('show')
+    saveBtn.classList.add('hide')
 
 
-function doneToDoItem(ele) {
-    ele.parentNode.classList.toggle("done")
-}
-
-function removeTodoItem(ele) {
-    ele.remove()
-    counter--
-    remove(ele)
+   
+    for(let i = 0; i <= child_eles.length; i++){
+        const ele = child_eles[i]
+        if(ele?.nodeName  == 'TD'){
+            ele.setAttribute('contenteditable', false)
+        }
+    }
 }
 
 function clear() {
